ba54bb451f8b153075f40502ecefdefa6144aa14
yikjunma <32088723+yikjunma@users.noreply.github.com>
2018-05-21 23:42:06 +0800

    collisions wonky

--- testApp/testApp/Scripts/Astroboy_1.lua
+++ testApp/testApp/Scripts/Astroboy_1.lua
@@ -1,6 +1,6 @@
-posX = 4.0
-posY = T1:getHeightAt(4.0,5.0)-22.97
-posZ = 5.0
+posX = 20.0
+posY = 240 --T1:getHeightAt(4.0,5.0)-22.97
+posZ = 20.0
 radius = 0.5
 height = 1.8
 mass = 60.0
@@ -9,5 +9,5 @@ Astronaut_1 = test3:convert(Astronaut_1)
 Astronaut_1:addModel(astroboy)
 astro1RigidBody = PhysicsManager:CreateCapsuleRigidBody(radius, height, mass, posX, posY, posZ)
 Astronaut_1:SetRigidBody(astro1RigidBody)
---Astronaut_1:updateObject(posX, posY, posZ)
+Astronaut_1:updateObject(posX, posY, posZ)
 --print ("Model finished ")
