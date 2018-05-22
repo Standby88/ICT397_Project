a3ade83feef7408e198c8536992de78c75813ae3
Standby88 <32767517@student.murdoch.edu.au>
2018-05-22 15:33:43 +0800

    Current best branch, stop working directly in master.

--- testApp/testApp/Scripts/Terrain.lua
+++ testApp/testApp/Scripts/Terrain.lua
@@ -2,7 +2,7 @@
 
 T1 = Factory:Create("WorldTerrain")
 T1 = test2:convert(T1);
-T1:setScallingFactor(2.0, 2.0, 2.0)
+T1:setScallingFactor(2.0, 1.0, 2.0)
 --don't need to translate mesh, rigidbody moves to mesh now
 --T1:updateObject(0.0, -100.0, 0.0)
 print ("terrain")
